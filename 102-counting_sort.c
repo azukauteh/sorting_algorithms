8b38d846b31976b5b1da021d60873979f355b841
azukauteh <aazure263@gmail.com>
2023-07-22 07:00:57 -0700

    a function that sorts an array of integers in ascending order using the Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -48,7 +48,7 @@ if (sorted == NULL)
 return;
 max = get_max(array, size);
 count = malloc(sizeof(int) * (max + 1));
-	
+
 if (count == NULL)
 {
 free(sorted);
@@ -57,10 +57,10 @@ return;
 
 for (i = 0; i < (max + 1); i++)
 count[i] = 0;
-	
+
 for (i = 0; i < (int)size; i++)
 count[array[i]] += 1;
-	
+
 for (i = 0; i < (max + 1); i++)
 count[i] += count[i - 1];
 print_array(count, max + 1);
